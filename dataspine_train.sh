5f0d2d162c3d126cca9cf3dfbb7fe25d40db8392
root <faisal.al.tameemi@gmail.com>
2019-06-06 21:10:05 +0000

    Setup

---
+++ dataspine_train.sh
@@ -0,0 +1,6 @@
+DATASPINE_MODEL_NAME=intentclassifier \
+DATASPINE_MODEL_TAG=v1 \
+DATASPINE_MODEL_TYPE=keras \
+DATASPINE_MODEL_RUNTIME=python \
+DATASPINE_MODEL_CHIP=cpu \
+  python dataspine_train.py
